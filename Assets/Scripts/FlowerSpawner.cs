daafdcb2e8f26e6de48312491a76f095faacdfb1
Yiyang <liuyiyang0929@gmail.com>
2024-01-27 00:39:39 +0800

    Added random spawning of flowers

---
+++ Assets/Scripts/FlowerSpawner.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerSpawner : MonoBehaviour
+{
+    public GameObject flowerPrefeb;
+    
+    public float maxSpawnInterval = 60f;
+    public float radius = 5f;
+    public Vector2 spawnAreaSize = new Vector2(10f, 5f);
+
+    private float timer = 0f;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        float randomInterval = Random.Range(0, 31);
+
+        if (timer >= maxSpawnInterval - randomInterval)
+        {
+            SpawnFlower();
+            timer = 0f;
+        }
+    }
+
+    void SpawnFlower()
+    {
+        float randomX = Random.Range(-spawnAreaSize.x / 2f, spawnAreaSize.x / 2f);
+        float randomY = Random.Range(-spawnAreaSize.y / 2f, spawnAreaSize.y / 2f);
+
+        Vector3 randomPosition = transform.position + new Vector3(randomX, randomY, 0f);
+
+        Instantiate(flowerPrefeb, randomPosition, Quaternion.identity);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, new Vector3(spawnAreaSize.x, spawnAreaSize.y, 0f));
+    }
+}
