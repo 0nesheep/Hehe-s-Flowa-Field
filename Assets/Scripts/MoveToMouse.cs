2d3b7f4b74d92bb19d715b8bc2abb57a668b5e7f
Yiyang <liuyiyang0929@gmail.com>
2024-01-27 14:57:28 +0800

    Added assets and animations

--- Assets/Scripts/MoveToMouse.cs
+++ Assets/Scripts/MoveToMouse.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 using System.Collections;
 public class moveToMouse : MonoBehaviour
 {
+    [SerializeField] private Animator animator = null;
+    private bool isMoving = false;
+   
     private Vector3 targetPosition;
 
     private float speed = 3f;
     private Rigidbody2D rb;
+    
 
     void Start()
     {
@@ -16,10 +20,29 @@ public class moveToMouse : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            isMoving = true;
+            animator.SetBool("isWalking", true);
             
         }
-        //transform.position = Vector2.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime, speed);
-        Vector3 direction = targetPosition - transform.position;
-        rb.MovePosition(rb.position + ((Vector2)direction * speed * Time.deltaTime));
+
+        if (isMoving)
+        {
+            //transform.position = Vector2.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime, speed);
+            Vector3 direction = targetPosition - transform.position;
+            rb.MovePosition(rb.position + ((Vector2)direction * speed * Time.deltaTime));
+            if (Vector3.Distance(transform.position, targetPosition) < 0.05f)
+            {
+                isMoving = false;
+            }
+        }
+    }
+
+    public void finishAnimation()
+    {
+        if (!isMoving)
+        {
+            animator.SetBool("isWalking", false);
+            Debug.Log("Stopped walking");
+        }
     }
 }
